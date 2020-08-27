ca9faaa0a36b7745ae5f8e3af5fdd6285c23daea
Leila-sheikh <70334377+Leila-sheikh@users.noreply.github.com>
2020-08-27 11:07:55 -0500

    We created the Rotate180Command class

---
+++ ImageFilterLibrary/Effect Commands/Rotate180Command.cs
@@ -0,0 +1,28 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageFilterLibrary.Effect_Commands
+{
+    public class Rotate180Command : IBitmapEffectCommand
+
+    {
+        private readonly Bitmap _image;
+
+        public Rotate180Command(Bitmap image)
+        {
+            _image = image;
+        }
+        public Bitmap Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bitmap Unexecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
