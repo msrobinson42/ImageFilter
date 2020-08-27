b9f8486cfe271ee3a0479a4a18d687d457107ca4
Zach Robinson <zprobinson@sio.midco.net>
2020-08-27 01:49:40 -0500

    Created IBitmapEffectCommand and a corresponding concrete implementation.

---
+++ ImageFilterLibrary/Effect Commands/Rotate90ClockwiseCommand.cs
@@ -0,0 +1,32 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageFilterLibrary.Effect_Commands
+{
+    class Rotate90ClockwiseCommand : IBitmapEffectCommand
+    {
+        private readonly Bitmap _image;
+
+        public Rotate90ClockwiseCommand(Bitmap image)
+        {
+            _image = image;
+        }
+
+        public Bitmap Execute()
+        {
+            _image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            return _image;
+        }
+
+        public Bitmap Unexecute()
+        {
+            _image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+            return _image;
+        }
+    }
+}
