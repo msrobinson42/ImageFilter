d2501d1e1af695962fce23e12b4bbb3a2010d44b
Zach Robinson <zprobinson@sio.midco.net>
2020-08-26 23:53:32 -0500

    Created a WinForms UI with a picture box, and allowed it to scale appropriately with the form.

---
+++ ImageFilterWinForms/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows.Forms;
+
+namespace ImageFilterWinForms
+{
+    partial class imageFilterView
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(imageFilterView));
+            this.picMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picMain
+            // 
+            this.picMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMain.BackColor = System.Drawing.Color.Black;
+            this.picMain.Image = ((System.Drawing.Image)(resources.GetObject("picMain.Image")));
+            this.picMain.Location = new System.Drawing.Point(0, 0);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(843, 401);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMain.TabIndex = 0;
+            this.picMain.TabStop = false;
+            // 
+            // imageFilterView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 502);
+            this.Controls.Add(this.picMain);
+            this.Name = "imageFilterView";
+            this.Text = "Image Filter";
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picMain;
+    }
+}
+
