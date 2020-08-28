3fcca21f9189a8dbfb6354f769c1bda1b235eff5
Zach Robinson <zprobinson@sio.midco.net>
2020-08-28 00:51:24 -0500

    Created a MosaicCommand and initialized the interface.

---
+++ ImageFilterLibrary/Effect Commands/MosaicEffectCommand.cs
@@ -0,0 +1,38 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageFilterLibrary.Effect_Commands
+{
+    public class MosaicEffectCommand : IBitmapEffectCommand
+    {
+        private readonly Bitmap _image;
+
+        public MosaicEffectCommand(Bitmap image)
+        {
+            _image = image;
+        }
+
+        public Bitmap Execute()
+        {
+            var newImage = new Bitmap(_image);
+
+
+
+
+            return default;
+        }
+
+        public Bitmap Unexecute()
+        {
+            return _image;
+        }
+
+        public void Dispose()
+        {
+            _image.Dispose();
+        }
+    }
+}
