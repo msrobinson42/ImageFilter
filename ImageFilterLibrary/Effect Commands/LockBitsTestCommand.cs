9828f4a2fac260bace6d3d77d1f2c1cb1f0d8a4b
Zach Robinson <zprobinson@sio.midco.net>
2020-08-30 21:07:41 -0500

    Created a TestCommand class.

---
+++ ImageFilterLibrary/Effect Commands/LockBitsTestCommand.cs
@@ -0,0 +1,38 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageFilterLibrary.Effect_Commands
+{
+    public class LockBitsTestCommand : IBitmapEffectCommand
+    {
+        private readonly Bitmap _image;
+
+        public LockBitsTestCommand(Bitmap image)
+        {
+            _image = image;
+        }
+
+        public Bitmap Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bitmap Unexecute()
+        {
+            return _image;
+        }
+
+        public IBitmapEffectCommand NewCommandFromCopy(Bitmap image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _image.Dispose();
+        }
+    }
+}
