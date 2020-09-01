b04202f2b30316f5312213ff115e9310c39d274e
Zach Robinson <zprobinson@sio.midco.net>
2020-09-01 13:30:44 -0500

    Implemented a Visitor pattern for creating/injecting commands into the Windows Form View.

---
+++ ImageFilterLibrary/CommandCreationVisitor/ICommandCreationVisitor.cs
@@ -0,0 +1,12 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageFilterLibrary.CommandCreationVisitor
+{
+    public interface ICommandCreationVisitor
+    {
+        IBitmapEffectCommand Visit(TestCommandCreationInformation info);
+    }
+}
---
+++ ImageFilterLibrary/CommandCreationVisitor/TestCommandCreationInformation.cs
@@ -0,0 +1,25 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using ImageFilterLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageFilterLibrary.CommandCreationVisitor
+{
+    public class TestCommandCreationInformation : ICommandCreationVisitee
+    {
+        public TestCommandCreationInformation(CommandFacade facade, int radius = 50)
+        {
+            Facade = facade;
+            Radius = radius;
+        }
+
+        public CommandFacade Facade { get; }
+        public int Radius { get; }
+
+        public IBitmapEffectCommand Accept(ICommandCreationVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+    }
+}
