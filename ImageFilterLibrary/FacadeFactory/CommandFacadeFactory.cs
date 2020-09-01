433c91d756980354bbda266f5085a5fbe38125c5
Zach Robinson <zprobinson@sio.midco.net>
2020-09-01 11:25:48 -0500

    Implemented a CommandFacadeFactory

---
+++ ImageFilterLibrary/FacadeFactory/CommandFacadeFactory.cs
@@ -0,0 +1,13 @@
+﻿using ImageFilterLibrary.Facades;
+using System.Drawing;
+
+namespace ImageFilterLibrary.FacadeFactory
+{
+    public class CommandFacadeFactory : ICommandFacadeFactory
+    {
+        public CommandFacade GetInstance(Bitmap image)
+        {
+            return new CommandFacade(image);
+        }
+    }
+}
---
+++ ImageFilterLibrary/FacadeFactory/ICommandFacadeFactory.cs
@@ -0,0 +1,13 @@
+﻿using ImageFilterLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageFilterLibrary.FacadeFactory
+{
+    public interface ICommandFacadeFactory
+    {
+        public CommandFacade GetInstance(Bitmap image);
+    }
+}
