32ef245885bca61b3b566fdd576014ba72560d69
Zach Robinson <zprobinson@sio.midco.net>
2020-09-01 11:25:04 -0500

    Created two empty commands for Leila to complete.

---
+++ ImageFilterLibrary/Effect Commands/PixelateCommand.cs
@@ -0,0 +1,41 @@
+﻿using ImageFilterLibrary.EffectCommands;
+using ImageFilterLibrary.Facades;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageFilterLibrary.Effect_Commands
+{
+    public class PixelateCommand : IBitmapEffectCommand
+    {
+        //TODO: Leila - Finish this class.
+        private readonly CommandFacade _facade;
+
+        public PixelateCommand(CommandFacade facade)
+        {
+            _facade = facade;
+        }
+
+        public Bitmap Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Bitmap Unexecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBitmapEffectCommand NewCommandFromCopy(Bitmap image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
