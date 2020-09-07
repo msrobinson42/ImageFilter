1113ce0bb197802cd7d319bf35c9bcac745f689e
Zach Robinson <zprobinson@sio.midco.net>
2020-09-07 16:21:30 -0500

    Cleanup

---
+++ ImageFilterLibrary/DisposableStateMachine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImageFilterLibrary
+{
+    public class DisposableStateMachine<T> where T : IDisposable
+    {
+        private readonly Stack<T> _undoStack = new Stack<T>();
+        private readonly Stack<T> _redoStack = new Stack<T>();
+
+        public DisposableStateMachine(T state)
+        {
+            State = state;
+        }
+
+        public T State { get; private set; }
+        public bool CanRedo => _redoStack.Any();
+        public bool CanUndo => _undoStack.Any();
+
+        public void Undo()
+        {
+            if (CanUndo)
+            {
+                _redoStack.Push(State);
+
+                var result = _undoStack.Pop();
+
+                State = result;
+            }
+        }
+
+        public void Redo()
+        {
+            if (CanRedo)
+            {
+                _undoStack.Push(State);
+
+                var result = _redoStack.Pop();
+
+                State = result;
+            }
+        }
+
+        private void ClearRedoStack()
+        {
+            while (_redoStack.Count > 0)
+            {
+                var bmap = _redoStack.Pop();
+                bmap.Dispose();
+            }
+        }
+
+        public void Update(T newState)
+        {
+            _undoStack.Push(State);
+
+            State = newState;
+
+            ClearRedoStack();
+
+        }
+    }
+}
--- ImageFilterLibrary/ImageEditorState.cs
+++ ImageFilterLibrary/ImageEditorState.cs
@@ -1,5 +1,4 @@
 ﻿using ImageProcessor;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -46,7 +45,7 @@ namespace ImageFilterLibrary
             }
         }
 
-        public void ClearRedoStack()
+        private void ClearRedoStack()
         {
             while(_redoStack.Count > 0)
             {
@@ -62,64 +61,6 @@ namespace ImageFilterLibrary
             Image = newImage;
 
             ClearRedoStack();
-        }
-    }
-
-    public class DisposableStateMachine<T> where T : IDisposable
-    {
-        private readonly Stack<T> _undoStack = new Stack<T>();
-        private readonly Stack<T> _redoStack = new Stack<T>();
-
-        public DisposableStateMachine(T state)
-        {
-            State = state;
-        }
-
-        public T State { get; private set; }
-        public bool CanRedo => _redoStack.Any();
-        public bool CanUndo => _undoStack.Any();
-
-        public void Undo()
-        {
-            if (CanUndo)
-            {
-                _redoStack.Push(State);
-
-                var result = _undoStack.Pop();
-
-                State = result;
-            }
-        }
-
-        public void Redo()
-        {
-            if (CanRedo)
-            {
-                _undoStack.Push(State);
-
-                var result = _redoStack.Pop();
-
-                State = result;
-            }
-        }
-
-        public void ClearRedoStack()
-        {
-            while (_redoStack.Count > 0)
-            {
-                var bmap = _redoStack.Pop();
-                bmap.Dispose();
-            }
-        }
-
-        public void Update(T newState)
-        {
-            _undoStack.Push(State);
-
-            State = newState;
-
-            ClearRedoStack();
-
         }
     }
 }
